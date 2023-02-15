d73f147a368133f0356db3b5ba20c62cf85a928f
Techno136 <mustafakeskin136@hotmail.com>
2023-02-16 00:22:33 +0300

    yeni

---
+++ app/src/main/java/com/example/ilkprojem/deneme.java
@@ -0,0 +1,12 @@
+package com.example.ilkprojem;
+
+public class deneme {
+
+    public static void main(String args[]) {
+
+        System.out.println("Merhaba Dünya");
+
+        // yeni
+
+    }
+}
